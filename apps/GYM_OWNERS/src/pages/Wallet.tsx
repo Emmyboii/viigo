f949f8e25222f5a9960c55ba0caa36e85bd7764c
Olukoya Emmanuel <olukoyae01@gmail.com>
2026-02-23 11:29:50 +0100

    Updated deployment error

--- apps/GYM_OWNERS/src/pages/Wallet.tsx
+++ apps/GYM_OWNERS/src/pages/Wallet.tsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+// import { useState } from "react";
 import WalletDetails from "../components/WalletDetails";
-import CreateWallet from "../components/CreateWallet";
+// import CreateWallet from "../components/CreateWallet";
 
 export default function Wallet() {
 
-    const [display, setDisplay] = useState<"details" | "create">("details");
+    // const [display, setDisplay] = useState<"details" | "create">("details");
 
     return (
         <div className="min-h-screen">
-            {display === "details" ? (
+            {/* {display === "details" ? ( */}
                 <WalletDetails />
-                // <WalletDetails setDisplay={setDisplay} />
-            ) : (
-                <CreateWallet />
-                // <CreateWallet setDisplay={setDisplay} />
-            )}
+                {/* // <WalletDetails setDisplay={setDisplay} /> */}
+            {/* ) : ( */}
+                {/* <CreateWallet /> */}
+                {/* // <CreateWallet setDisplay={setDisplay} /> */}
+            {/* )} */}
         </div>
     )
 }
